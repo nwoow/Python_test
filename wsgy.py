1fd9f02e0059ad3bcdbece6eab5127c13e74f4b9
Nilay Singh <nilayy1234@gmail.com>
2019-05-04 22:08:34 +0530

    added

--- wsgy.py
+++ wsgy.py
@@ -1,12 +1,11 @@
 def application(environ,start_response):
     status = '200 OK'
-    html = '<html>\n' \
-           '<body>\n' \
-           '<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">\n' \
-           'Welcome to mod_wsgi Test Page\n' \
-           '</div>\n' \
-           '</body>\n' \
-           '</html>\n'
-    response_header = [('Content-type','text/html')]
+    html = {
+        'chart': {
+            'result': "eer",
+            'error':"hhh"
+        }
+    }
+    response_header = [('Content-type','json')]
     start_response(status,response_header)
     return [html]
