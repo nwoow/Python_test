55c8a7cb5a86b713679e3c82c08b7dbfe7a984c7
Nilay Singh <nilayy1234@gmail.com>
2019-05-04 16:18:30 +0530

    added

--- mysql.py
+++ mysql.py
@@ -9,22 +9,16 @@ db = pymysql.connect("localhost","root","DxyAY1JLS9c$q@WKoLG!8rs","pytest" )
 cursor = db.cursor()
 
 # Prepare SQL query to INSERT a record into the database.
-# sql = "INSERT INTO STOCK(symbol, \
-#    delayedPrice, high, low, delayedSize,delayedPriceTime,processedTime) \
-#    VALUES ('%s', '%s', '%s', '%s', '%s','%s','%s' )" % \
-#    ('Mac', 'Mohan', 20, 'M', 2000)
-add_stock = ("INSERT INTO STOCK "
-               "(symbol,delayedPrice, high, low, delayedSize,delayedPriceTime,processedTime) "
-               "VALUES (%s, %s, %s, %s, %s, %s, %s)")
-data_employee = ('Geert', 123, 123,3232, 23232,3434,34343)              
+sql = """INSERT INTO STOCK(symbol,delayedPrice, high, low, delayedSize,delayedPriceTime,processedTime)
+   VALUES ('Mac', 34, 20, 3434, 2000,34343,4343)"""
 try:
    # Execute the SQL command
-    cursor.execute(add_stock, data_employee)
+   cursor.execute(sql)
    # Commit your changes in the database
-    db.commit()
+   db.commit()
 except:
    # Rollback in case there is any error
-    db.rollback()
+   db.rollback()
 
 # disconnect from server
 db.close()
