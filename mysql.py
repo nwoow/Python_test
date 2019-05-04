9fb4d54ea1cf2280a18c98a70556ebb3a7760201
Nilay Singh <nilayy1234@gmail.com>
2019-05-04 16:07:53 +0530

    added

--- mysql.py
+++ mysql.py
@@ -8,20 +8,24 @@ db = pymysql.connect("localhost","root","DxyAY1JLS9c$q@WKoLG!8rs","pytest" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-# Drop table if it already exist using execute() method.
-# cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-
-# Create table as per requirement
-sql = """CREATE TABLE Stock (
-   symbol  CHAR(20) NOT NULL,
-   delayedPrice  FLOAT,
-   high FLOAT,  
-   low FLOAT,
-   delayedSize INT,
-   delayedPriceTime INT,
-   processedTime INT )"""
-
-cursor.execute(sql)
+# Prepare SQL query to INSERT a record into the database.
+# sql = "INSERT INTO STOCK(symbol, \
+#    delayedPrice, high, low, delayedSize,delayedPriceTime,processedTime) \
+#    VALUES ('%s', '%s', '%s', '%s', '%s','%s','%s' )" % \
+#    ('Mac', 'Mohan', 20, 'M', 2000)
+add_stock = ("INSERT INTO STOCK "
+               "(symbol,delayedPrice, high, low, delayedSize,delayedPriceTime,processedTime) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+ data_employee = ('Geert', 123, 123,3232, 23232,3434,34343)              
+try:
+   # Execute the SQL command
+    cursor.execute(add_stock, data_employee)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
 
 # disconnect from server
 db.close()
+
