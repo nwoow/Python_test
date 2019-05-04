1299e9daacad3d9e20c63fe4d7a9244175547851
Nilay Singh <nilayy1234@gmail.com>
2019-05-04 23:40:28 +0530

    Added

--- django/stock_test/stock/views.py
+++ django/stock_test/stock/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = "dddd"
+    template = loader.get_template('stock/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
