d6d460e47dcde2de6d00683197d9538e321bbdc7
Nilay Singh <nilayy1234@gmail.com>
2019-05-04 16:19:41 +0530

    added

--- mysql.py
+++ mysql.py
@@ -11,15 +11,16 @@ cursor = db.cursor()
 # Prepare SQL query to INSERT a record into the database.
 sql = """INSERT INTO STOCK(symbol,delayedPrice, high, low, delayedSize,delayedPriceTime,processedTime)
    VALUES ('Mac', 34, 20, 3434, 2000,34343,4343)"""
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Commit your changes in the database
-   db.commit()
-except:
-   # Rollback in case there is any error
-   db.rollback()
+# try:
+#    # Execute the SQL command
+#    cursor.execute(sql)
+#    # Commit your changes in the database
+#    db.commit()
+# except:
+#    # Rollback in case there is any error
+#    db.rollback()
 
 # disconnect from server
+cursor.execute(sql)
 db.close()
 
