cb1e8b5c13da5f13aea0a86cedc5799c01f2a44d
Nilay Singh <nilayy1234@gmail.com>
2019-05-07 00:23:55 +0530

    Added

--- django/stock_test/stock/views.py
+++ django/stock_test/stock/views.py
@@ -22,10 +22,11 @@ def index(request):
 def json(request):
     qs = Stock.objects.all()
     df = read_frame(qs)
+    df2 = df.groupby(['symbol']).plot(kind='line',x="processedTime", y="delayedPrice")
     context = {
         'latest_question_list': "error",  
     }
     template = loader.get_template('stock/json.html')
-    return JsonResponse({'foo': df})
+    return JsonResponse({'foo': df2})
 
     # return HttpResponse(template.render(context, request))    
