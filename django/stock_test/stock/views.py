aca440be99cf41cb5b16c2a3623fb7783db702eb
Nilay Singh <nilayy1234@gmail.com>
2019-05-06 23:50:08 +0530

    Added

--- django/stock_test/stock/views.py
+++ django/stock_test/stock/views.py
@@ -9,7 +9,7 @@ from django_pandas.io import read_frame
 def index(request):
     latest_question_list = Stock.objects.all()
     qs = Stock.objects.all()
-    df = qs.to_dataframe()
+    df = qs.to_dataframe(fieldnames=['symbol', 'delayedprice', 'high'])
     template = loader.get_template('stock/index.html')
     context = {
         'latest_question_list': latest_question_list,
